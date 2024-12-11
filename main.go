169ba4a1f2f544ffaa36590d7d25146c7678f45a
xx-qq <sshyta@mail.ru>
2024-12-11 23:00:50 +0300

    Commit

--- main.go
+++ main.go
@@ -11,29 +11,46 @@ import (
 )
 
 func main() {
-	setLogPath("logs/app.log")
+	beego.LoadAppConfig("ini", "conf/app.conf")
+	beego.SetStaticPath("/static", "static")
+
 	beego.BConfig.Listen.HTTPAddr = "localhost"
 	beego.BConfig.Listen.HTTPPort = 8181
 	orm.RegisterDriver("db", orm.DRMySQL)
 	// строка подключения: пользователь:пароль@tcp(хост:порт)/имя_базы_данных
 	orm.RegisterDataBase("default", "postgres", "user=postgres password=467912 host=127.0.0.1 port=5432 dbname=web_cloud_storage sslmode=disable")
 
+	setLogPath("logs/app.log")
 	beego.Run()
 }
 
-func setLogPath(string) {
-	LogPath := "logs"
-	if _, err := os.Stat(LogPath); os.IsNotExist(err) {
-		err := os.Mkdir(LogPath, os.ModePerm)
+func setLogPath(logFilePath string) {
+	logpath := logFilePath[:len(logFilePath)-len("/"+getFileName(logFilePath))]
+	if _, err := os.Stat(logpath); os.IsNotExist(err) {
+		err := os.Mkdir(logpath, os.ModePerm)
 		if err != nil {
 			log.Fatal("Не удалось создать папку %v", err)
 		}
 	}
 	beego.BConfig.Log.FileLineNum = true
+	beego.BConfig.Log.AccessLogs = true
 	beego.BConfig.Log.Outputs = map[string]string{
 		"console": "",
-		"file":    LogPath + "/app.log",
+		"file":    logFilePath,
 	}
 
-	log.Printf(LogPath)
+	log.Printf(logFilePath)
+}
+
+func getFileName(filePath string) string {
+	if len(filePath) == 0 {
+		return ""
+	}
+
+	for i := len(filePath) - 1; i >= 0; i-- {
+		if filePath[i] == '/' || filePath[i] == '\\' {
+			return filePath[i+1:]
+		}
+	}
+	return filePath
 }
--- views/storage.tpl
+++ views/storage.tpl
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manager Files</title>
+    <title>storage</title>
     <link rel="stylesheet" href="/static/css/storage.css">
 </head>
 <body>
